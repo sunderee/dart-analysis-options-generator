794ec847989fe2aaee6a9809cc7969a4ca847cb3
Peter Aleksander Bizjak <peteralexbizjak@protonmail.com>
2023-08-13 11:37:53 +0200

    Complete the scraper part

---
+++ lib/models.dart
@@ -0,0 +1,34 @@
+import 'package:meta/meta.dart';
+
+enum StyleEnum {
+  core,
+  recommended,
+  flutter;
+
+  static StyleEnum? fromSRCString(String input) {
+    if (input.endsWith('style-core.svg')) {
+      return StyleEnum.core;
+    } else if (input.endsWith('style-recommended.svg')) {
+      return StyleEnum.recommended;
+    } else if (input.endsWith('style-flutter.svg')) {
+      return StyleEnum.flutter;
+    } else {
+      return null;
+    }
+  }
+}
+
+@immutable
+final class LinterRuleModel {
+  final String title;
+  final String description;
+  final Set<StyleEnum> styles;
+  final String? status;
+
+  const LinterRuleModel(
+    this.title,
+    this.description,
+    this.styles,
+    this.status,
+  );
+}
